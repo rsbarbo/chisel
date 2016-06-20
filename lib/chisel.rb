8c0b61661732349e6d6577e3cf8cb83973c8ff81
Raphael Barbo <rsbarbo@gmail.com>
2016-06-20 17:55:24 -0600

    test passing, methods header_parser created and paragraph_parser

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -5,10 +5,13 @@ class Chisel
 def header_parser(text)
   if text.include?("#")
     counter = text.count("#")
-    text.gsub!("#", "<h#{counter}>") + "</h#{counter}>"
-  else
+    text.gsub("#{"#" * counter}", "<h#{counter}>") + "</h#{counter}>"  else
     text.insert(0, "<p>") + "</p>"
   end
+end
+
+def paragraph_parser(text)
+  "<p>\n" + (text) + "\n</p>\n"
 end
 
 end
--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -14,8 +14,20 @@ def test_turn_pound_into_header
   assert_equal "<h1>Raphael</h1>", chisel.header_parser("#Raphael")
 end
 
+def test_turn_pound_into_header1_2_3_4_etc
+  assert_equal "<h2>Raphael</h2>", chisel.header_parser("##Raphael")
+end
+
 def test_text_into_paragraph
   assert_equal "<p>Raphael</p>", chisel.header_parser("Raphael")
+end
+
+def test_text_into_same_paragraph_even_with_multiple_lines
+  assert_equal "<p>Raphael\n You are the best</p>", chisel.header_parser("Raphael\n You are the best")
+end
+
+def test_test
+  assert_equal "<p>\nRaphael Raphael Raphael\n</p>\n", chisel.paragraph_parser("Raphael Raphael Raphael")
 end
 
 end
