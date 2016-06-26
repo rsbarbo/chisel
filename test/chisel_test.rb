c0a4f5fa694e03493b5f01c122ec5d0f3b926575
Raphael Barbo <rsbarbo@gmail.com>
2016-06-26 11:46:56 -0600

    indented

--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -6,35 +6,34 @@ require "pry"
 
 class ChiselFileTest<Minitest::Test
 
-attr_reader :chisel
+  attr_reader :chisel
 
-def setup
-  @chisel = ChiselFile.new
-end
+  def setup
+    @chisel = ChiselFile.new
+  end
 
-def test_turn_pound_into_header
-  assert_equal "<h1>Raphael</h1>", chisel.parser("#Raphael")
-end
+  def test_turn_pound_into_header
+    assert_equal "<h1>Raphael</h1>", chisel.parser("#Raphael")
+  end
 
-def test_turn_pound_into_header2
-  assert_equal "<h2>Raphael</h2>", chisel.parser("##Raphael")
-end
+  def test_turn_pound_into_header2
+    assert_equal "<h2>Raphael</h2>", chisel.parser("##Raphael")
+  end
 
-def test_turn_pound_into_header3
-  assert_equal "<h3>Raphael</h3>", chisel.parser("###Raphael")
-end
+  def test_turn_pound_into_header3
+    assert_equal "<h3>Raphael</h3>", chisel.parser("###Raphael")
+  end
 
+  def test_text_into_paragraph
+    assert_equal "<p>\nRaphael\n</p>\n", chisel.parser("Raphael")
+  end
 
-def test_text_into_paragraph
-  assert_equal "<p>\nRaphael\n</p>\n", chisel.parser("Raphael")
-end
+  def test_text_into_same_paragraph_even_with_multiple_lines
+    assert_equal "<p>\nRaphael\n You are the best\n</p>\n", chisel.parser("Raphael\n You are the best")
+  end
 
-def test_text_into_same_paragraph_even_with_multiple_lines
-  assert_equal "<p>\nRaphael\n You are the best\n</p>\n", chisel.parser("Raphael\n You are the best")
-end
-
-def test_paragraph
-  assert_equal "<p>\nRaphael Raphael Raphael\n</p>\n", chisel.parser("Raphael Raphael Raphael")
-end
+  def test_paragraph
+    assert_equal "<p>\nRaphael Raphael Raphael\n</p>\n", chisel.parser("Raphael Raphael Raphael")
+  end
 
 end
--- test/tagging_test.rb
+++ test/tagging_test.rb
@@ -12,7 +12,6 @@ class TaggingTest<Minitest::Test
     @tagging = Tagging.new
   end
 
-
   def test_emphasized_text
     assert_equal "<em>", tagging.translate_to_tag("*")
   end
@@ -20,7 +19,6 @@ class TaggingTest<Minitest::Test
   def test_strong_text
     assert_equal "<strong>", tagging.translate_to_tag("**")
   end
-
 
   def test_translate_strong_text_in_the_middle_of_the_phrase
     assert_equal "Hello <strong>Raphael", tagging.translate_to_tag("Hello **Raphael")
